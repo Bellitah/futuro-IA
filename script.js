e9d2eb9c4e60367cf3ca014fb45c7ef8ff45c44c
Isabella Lopes Pereira <105372986+Bellitah@users.noreply.github.com>
2024-06-13 08:15:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const caixaPrincipal = document.querySelector(".caixa-principal"); const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas"); const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaAlternativas = document.querySelector(".caixa-alternativas"); const caixaPrincipal = document.querySelector(".caixa-principal");
+const textoResultado = document.querySelector(".texto-resultado"); const caixaPrincipal = document.querySelector(".caixa-principal");
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+
+];
